20587da58f8aa115ceb74250da71949d5a702c43
Trupti-c-6 <truptichimms@gmail.com>
2026-09-08 13:40:42 +0530

    Fix payment order ID

---
+++ site/js/lib/order-id.js
@@ -0,0 +1,10 @@
+function generateOrderId() {
+  const timestamp = Date.now().toString(36).toUpperCase();
+
+  const random = Math.random()
+    .toString(36)
+    .slice(2, 7)
+    .toUpperCase();
+
+  return `BOM-${timestamp}-${random}`;
+}
